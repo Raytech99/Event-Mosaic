52d2d51d49d0045e725234eafac60a418c10ab6d
Rayyan Jamil <raymanatl@gmail.com>
2025-03-27 06:30:39 -0400

    Allow requests from mobile platform

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Configure CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://eventmosaic.net'],
+  origin: '*', // Allow all origins during development
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
