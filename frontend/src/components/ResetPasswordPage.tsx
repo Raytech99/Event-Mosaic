ba1ab86e4c3dec40b6924a127693909b0195327e
mayacou <mayalynnx@hotmail.com>
2025-04-01 15:31:17 -0400

    removed unused function

--- frontend/src/components/ResetPasswordPage.tsx
+++ frontend/src/components/ResetPasswordPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
 import { buildPath, API_ROUTES } from '../utils/api';
 
